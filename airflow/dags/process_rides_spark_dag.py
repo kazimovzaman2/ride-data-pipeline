cf4733e3ed0960fe7634f640761bde7851861d14
kazimovzaman2 <kazimovzaman2@gmail.com>
2025-09-03 15:47:54 +0400

    Add process rides DAG and update docker-compose for Spark jobs

---
+++ airflow/dags/process_rides_spark_dag.py
@@ -0,0 +1,38 @@
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    "owner": "zaman",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=2),
+}
+
+with DAG(
+    dag_id="process_rides_spark_dag",
+    default_args=default_args,
+    description="Incremental processing of taxi rides using Spark",
+    schedule_interval="*/10 * * * *",
+    start_date=datetime(2025, 9, 3),
+    catchup=False,
+    tags=["spark", "etl", "taxi"],
+) as dag:
+
+    process_rides = SparkSubmitOperator(
+        task_id="process_rides_task",
+        application="/opt/spark_jobs/process_ride_data.py",
+        conn_id="spark_default",
+        verbose=True,
+        name="process_rides_job",
+        conf={
+            "spark.master": "spark://spark-master:7077",
+            "spark.driver.memory": "2g",
+            "spark.executor.memory": "2g",
+            "spark.jars": "/opt/spark/jars/postgresql-42.6.0.jar",
+        },
+    )
+
+    process_rides
