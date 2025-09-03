f69528ab7ec8a7a7c0f1688d1b6668d62914be40
kazimovzaman2 <kazimovzaman2@gmail.com>
2025-09-03 15:54:30 +0400

    Refactor default_args and SparkSubmitOperator configuration in process rides DAG

--- airflow/dags/process_rides_spark_dag.py
+++ airflow/dags/process_rides_spark_dag.py
@@ -5,8 +5,7 @@ from datetime import datetime, timedelta
 default_args = {
     "owner": "zaman",
     "depends_on_past": False,
-    "email_on_failure": False,
-    "email_on_retry": False,
+    "start_date": datetime(2025, 9, 3),
     "retries": 1,
     "retry_delay": timedelta(minutes=2),
 }
@@ -27,11 +26,15 @@ with DAG(
         conn_id="spark_default",
         verbose=True,
         name="process_rides_job",
-        conf={
-            "spark.master": "spark://spark-master:7077",
-            "spark.driver.memory": "2g",
-            "spark.executor.memory": "2g",
-            "spark.jars": "/opt/spark/jars/postgresql-42.6.0.jar",
+        **{
+            "spark_binary": "/opt/spark/bin/spark-submit",
+            "application_args": [],
+            "conf": {
+                "spark.master": "spark://spark-master:7077",
+                "spark.driver.memory": "2g",
+                "spark.executor.memory": "2g",
+                "spark.jars": "/opt/spark/jars/postgresql-42.6.0.jar",
+            },
         },
     )
 
